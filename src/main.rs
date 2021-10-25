43438643b8950635a20d4d3133e3e8f258bd922d
Luis Pedro Coelho <luis@luispedro.org>
2021-10-25 16:26:14 +0200

    ENH Read two files & do basic compare

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use std::env::args;
 use std::fs;
+use std::cmp::max;
+
+fn compare(text1: String, text2: String) {
+    let lines1 : Vec<&str> = text1.split("\n").collect();
+    let lines2 : Vec<&str> = text2.split("\n").collect();
+    let max_len = max(lines1.len(), lines2.len());
+    for ix in 0..max_len {
+        if ix >= lines1.len() {
+            println!("+1");
+        } else if ix >= lines2.len() {
+            println!("+2");
+        } else if lines1[ix] == lines2[ix] {
+            println!("=");
+        } else {
+            println!("~");
+        }
+    }
+}
 
 fn main() {
     let fnames : Vec<String> = args().collect();
@@ -10,7 +28,6 @@ fn main() {
     let fname2 = &fnames[2];
     let text1 = fs::read_to_string(fname1).unwrap();
     let text2 = fs::read_to_string(fname2).unwrap();
-    println!("Number of characters (1): {}", text1.len());
-    println!("Number of characters (2): {}", text2.len());
+    compare(text1, text2);
 }
 
