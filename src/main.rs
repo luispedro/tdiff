5db158130e3b148f2133328b64bd22b63bd0372d
Luis Pedro Coelho <luis@luispedro.org>
2021-10-25 14:23:20 +0200

    ENH Count number of lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![feature(stdin_forwarders)]
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let input : Result<Vec<String>, _> =
+        io::stdin()
+        .lines()
+        .collect();
+
+    match input {
+        Ok(lines) =>
+            println!("Number of lines: {}", lines.len()),
+        Err(err) =>
+            println!("Error!: {}", err),
+    }
 }
