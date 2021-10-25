a0abee28d5b6f976bad099c1a24c45d143b364d8
Luis Pedro Coelho <luis@luispedro.org>
2021-10-25 14:45:11 +0200

    ENH Read two files & count characters

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-#![feature(stdin_forwarders)]
-use std::io;
+use std::env::args;
+use std::fs;
 
 fn main() {
-    let input : Result<Vec<String>, _> =
-        io::stdin()
-        .lines()
-        .collect();
-
-    match input {
-        Ok(lines) =>
-            println!("Number of lines: {}", lines.len()),
-        Err(err) =>
-            println!("Error!: {}", err),
+    let fnames : Vec<String> = args().collect();
+    if fnames.len() != 3 {
+        panic!("Wrong number of arguments");
     }
+    let fname1 = &fnames[1];
+    let fname2 = &fnames[2];
+    let text1 = fs::read_to_string(fname1).unwrap();
+    let text2 = fs::read_to_string(fname2).unwrap();
+    println!("Number of characters (1): {}", text1.len());
+    println!("Number of characters (2): {}", text2.len());
 }
+
