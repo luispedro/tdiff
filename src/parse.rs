cc510857628916fb87316129aa56787e20e77a28
Luis Pedro Coelho <luis@luispedro.org>
2021-10-27 12:22:05 +0200

    TST Add unit testing

--- src/parse.rs
+++ src/parse.rs
@@ -10,5 +10,29 @@ pub fn sentences(text : String) -> Vec<String> {
     return text
         .split(".")
         .map(normalize_sentence)
+        .filter(|s| { s.len() > 0 })
         .collect();
+}
+
+#[test]
+fn test_trim_space() {
+    assert!(
+        normalize_sentence("Hello world")
+        == "Hello world");
+    assert!(
+        normalize_sentence("   Hello world    ")
+        == "Hello world");
+    assert!(
+        normalize_sentence("   Hello    world    ")
+        == "Hello world");
+    assert!(
+        normalize_sentence("\tHello\n\nworld    ")
+        == "Hello world");
+}
+
+#[test]
+fn test_sentences() {
+    assert!(
+        sentences("Hello world. My name is tdiff.\n\n\n".to_string()).len()
+        == 2);
 }
