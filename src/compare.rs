7d6fd84d5cc660a729ca1459ccc63987cff01ae9
Luis Pedro Coelho <luis@luispedro.org>
2021-10-27 12:13:32 +0200

    ENH Add some comparison infrastructure

--- src/compare.rs
+++ src/compare.rs
@@ -1,20 +1,52 @@
 use std::cmp::max;
 
-pub fn compare(text1: String, text2: String) {
-    let lines1 : Vec<String> = super::parse::sentences(text1);
-    let lines2 : Vec<String> = super::parse::sentences(text2);
-    let max_len = max(lines1.len(), lines2.len());
+enum DiffElement {
+    Equal(String),
+    Insert1(String),
+    Insert2(String),
+    Different(String, String),
+}
+
+fn gen_diff(text1: Vec<String>, text2: Vec<String>) -> Vec<DiffElement> {
+    let mut res = Vec::new();
+    let max_len = max(text1.len(), text2.len());
     for ix in 0..max_len {
-        if ix >= lines1.len() {
-            println!("+1");
-        } else if ix >= lines2.len() {
-            println!("+2");
-        } else if lines1[ix] == lines2[ix] {
-            println!("=");
+        if ix >= text1.len() {
+            res.push(DiffElement::Insert1(text2[ix].clone()));
+        } else if ix >= text2.len() {
+            res.push(DiffElement::Insert2(text1[ix].clone()));
+        } else if text1[ix] == text2[ix] {
+            res.push(DiffElement::Equal(text1[ix].clone()));
         } else {
-            println!("~");
-            println!("    {}", lines1[ix]);
-            println!("    {}", lines2[ix]);
+            res.push(DiffElement::Different(text1[ix].clone(), text2[ix].clone()));
         }
+    }
+    return res;
+}
+
+fn show_diff(el : &DiffElement) {
+    match el {
+        DiffElement::Equal(s) => {
+            println!("= {}", s);
+        }
+        DiffElement::Insert1(s) => {
+            println!("+ {}", s);
+        }
+        DiffElement::Insert2(s) => {
+            println!("- {}", s);
+        }
+        DiffElement::Different(s1, s2) => {
+            println!("! {}", s1);
+            println!("! {}", s2);
+        }
+    }
+}
+
+pub fn compare(text1: String, text2: String) {
+    let sentences1 : Vec<String> = super::parse::sentences(text1);
+    let sentences2 : Vec<String> = super::parse::sentences(text2);
+    let diff = gen_diff(sentences1, sentences2);
+    for el in diff {
+        show_diff(&el);
     }
 }
