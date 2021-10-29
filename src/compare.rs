6a4174899f7c88dd7326651eef1f9739156700a6
Luis Pedro Coelho <luis@luispedro.org>
2021-10-29 16:02:09 +0200

    RFCT Generalize edit_distance
    In principle could be used with any type

--- src/compare.rs
+++ src/compare.rs
@@ -12,13 +12,7 @@ enum DiffElement<T> {
 }
 
 
-fn compare_sentences(text1: Vec<&str>, text2: Vec<&str>) -> (u64, Vec<DiffElement<String>>) {
-    if text1 == text2 {
-        return (0, text1
-                    .iter()
-                    .map(|s| { DiffElement::Equal(s.to_string()) })
-                    .collect());
-    }
+fn edit_distance<T: PartialEq + Clone>(text1: Vec<T>, text2: Vec<T>) -> (u64, Vec<DiffElement<T>>) {
     let mut table : Vec<Vec<u64>> = Vec::new();
     table.resize_with(text1.len() + 1, || {
         let mut v = Vec::new();
@@ -47,25 +41,38 @@ fn compare_sentences(text1: Vec<&str>, text2: Vec<&str>) -> (u64, Vec<DiffElemen
     while p1 > 0 || p2 > 0 {
         let val = table[p1][p2];
         if p1 > 0 && p2 > 0 && val == table[p1-1][p2-1] && text1[p1-1] == text2[p2 - 1] {
-            walk.push(DiffElement::Equal(text1[p1-1].to_string()));
+            walk.push(DiffElement::Equal(text1[p1-1].clone()));
             p1 -= 1;
             p2 -= 1;
         } else if p1 > 0 && p2 > 0 && val == table[p1-1][p2-1] + 1 && text1[p1-1] != text2[p2 - 1] {
-            walk.push(DiffElement::Different(text1[p1-1].to_string(), text2[p2 - 1].to_string()));
+            walk.push(DiffElement::Different(text1[p1-1].clone(), text2[p2 - 1].clone()));
             p1 -= 1;
             p2 -= 1;
         } else if p1 > 0 && val == table[p1-1][p2] + 1 {
-            walk.push(DiffElement::Insert1(text1[p1-1].to_string()));
+            walk.push(DiffElement::Insert1(text1[p1-1].clone()));
             p1 -= 1;
         } else {
             assert!(val == table[p1][p2-1] + 1);
-            walk.push(DiffElement::Insert2(text2[p2-1].to_string()));
+            walk.push(DiffElement::Insert2(text2[p2-1].clone()));
             p2 -= 1;
         }
     }
     walk.reverse();
     (table[text1.len()][text2.len()], walk)
 }
+
+fn compare_sentences(text1: &Sentence, text2: &Sentence) -> (u64, Vec<DiffElement<String>>) {
+    if text1 == text2 {
+        return (0, text1
+                    .0
+                    .split(" ")
+                    .map(|s| { DiffElement::Equal(s.to_string()) })
+                    .collect());
+    }
+    return edit_distance(text1.0.split(" ").map(|s| { s.to_string() }).collect(),
+                         text2.0.split(" ").map(|s| { s.to_string() }).collect())
+}
+
 
 fn print_edit_script(es : &Vec<DiffElement<String>>) {
     for el in es {
@@ -79,35 +86,35 @@ fn print_edit_script(es : &Vec<DiffElement<String>>) {
     println!("");
 }
 
-fn gen_diff(text1: Vec<Sentence>, text2: Vec<Sentence>) -> Vec<DiffElement<String>> {
+fn gen_diff(text1: Vec<Sentence>, text2: Vec<Sentence>) -> Vec<DiffElement<Sentence>> {
     let mut res = Vec::new();
     let max_len = max(text1.len(), text2.len());
     for ix in 0..max_len {
         if ix >= text1.len() {
-            res.push(DiffElement::Insert1(text2[ix].0.clone()));
+            res.push(DiffElement::Insert1(text2[ix].clone()));
         } else if ix >= text2.len() {
-            res.push(DiffElement::Insert2(text1[ix].0.clone()));
+            res.push(DiffElement::Insert2(text1[ix].clone()));
         } else if text1[ix] == text2[ix] {
-            res.push(DiffElement::Equal(text1[ix].0.clone()));
+            res.push(DiffElement::Equal(text1[ix].clone()));
         } else {
-            res.push(DiffElement::Different(text1[ix].0.clone(), text2[ix].0.clone()));
+            res.push(DiffElement::Different(text1[ix].clone(), text2[ix].clone()));
         }
     }
     return res;
 }
 
-fn show_diff(el : &DiffElement<String>) {
+fn show_diff(el : &DiffElement<Sentence>) {
     match el {
         DiffElement::Equal(_) => {
         }
         DiffElement::Insert1(s) => {
-            println!("+ {}", s.green());
+            println!("+ {}", s.0.green());
         }
         DiffElement::Insert2(s) => {
-            println!("- {}", s.red());
+            println!("- {}", s.0.red());
         }
         DiffElement::Different(s1, s2) => {
-            print_edit_script(&compare_sentences(s1.split(" ").collect(), s2.split(" ").collect()).1);
+            print_edit_script(&compare_sentences(s1, s2).1);
         }
     }
 }
@@ -123,16 +130,17 @@ pub fn compare(text1: String, text2: String) {
 
 #[test]
 fn test_compare_sentences() {
+    let to_s = |s:&str| { Sentence(s.to_string()) };
     assert!(
-        compare_sentences("Hello world".split(" ").collect(), "Hello world".split(" ").collect()).0
+        compare_sentences(&to_s("Hello world"), &to_s("Hello world")).0
         == 0);
     assert!(
-        compare_sentences("Hello world".split(" ").collect(), "Hello cruel world".split(" ").collect()).0
+        compare_sentences(&to_s("Hello world"), &to_s("Hello cruel world")).0
         == 1);
     assert!(
-        compare_sentences("Hello world".split(" ").collect(), "Goodbye cruel world".split(" ").collect()).0
+        compare_sentences(&to_s("Hello world"), &to_s("Goodbye cruel world")).0
         == 2);
     assert!(
-        compare_sentences("Hello world".split(" ").collect(), "Goodbye mediocre Paris".split(" ").collect()).0
+        compare_sentences(&to_s("Hello world"), &to_s("Goodbye mediocre Paris")).0
         == 3);
 }
