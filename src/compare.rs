ed4e7d3f63c1b251edb9fe09d67324902407571a
Luis Pedro Coelho <luis@luispedro.org>
2021-10-29 14:16:12 +0200

    ENH Generate the "edit script"

--- src/compare.rs
+++ src/compare.rs
@@ -1,6 +1,7 @@
 use std::cmp::{min, max};
 use super::parse::*;
 
+#[derive(Debug)]
 enum DiffElement {
     Equal(String),
     Insert1(String),
@@ -8,7 +9,7 @@ enum DiffElement {
     Different(String, String),
 }
 
-fn compare_sentences(text1: Vec<&str>, text2: Vec<&str>) -> u64 {
+fn compare_sentences(text1: Vec<&str>, text2: Vec<&str>) -> (u64, Vec<DiffElement>) {
     let mut table : Vec<Vec<u64>> = Vec::new();
     table.resize_with(text2.len() + 1, || {
         let mut v = Vec::new();
@@ -31,7 +32,30 @@ fn compare_sentences(text1: Vec<&str>, text2: Vec<&str>) -> u64 {
                             )
         }
     }
-    table[text1.len() - 1][text2.len() - 1]
+    let mut walk = Vec::new();
+    let mut p1 = text1.len();
+    let mut p2 = text2.len();
+    while p1 > 0 || p2 > 0 {
+        let val = table[p1][p2];
+        if val == table[p1-1][p2-1] && text1[p1-1] == text2[p2 - 1] {
+            walk.push(DiffElement::Equal(text1[p1-1].to_string()));
+            p1 -= 1;
+            p2 -= 1;
+        } else if val == table[p1-1][p2-1] + 1 && text1[p1-1] != text2[p2 - 1] {
+            walk.push(DiffElement::Different(text1[p1-1].to_string(), text2[p2 - 1].to_string()));
+            p1 -= 1;
+            p2 -= 1;
+        } else if val == table[p1-1][p2] + 1 {
+            walk.push(DiffElement::Insert1(text1[p1-1].to_string()));
+            p1 -= 1;
+        } else {
+            assert!(val == table[p1-1][p2] + 1);
+            walk.push(DiffElement::Insert2(text2[p2-1].to_string()));
+            p2 -= 1;
+        }
+    }
+    walk.reverse();
+    (table[text1.len()][text2.len()], walk)
 }
 
 fn gen_diff(text1: Vec<Sentence>, text2: Vec<Sentence>) -> Vec<DiffElement> {
@@ -65,7 +89,7 @@ fn show_diff(el : &DiffElement) {
         DiffElement::Different(s1, s2) => {
             println!("! {}", s1);
             println!("! {}", s2);
-            println!("D:  {}", compare_sentences(s1.split(" ").collect(), s2.split(" ").collect()));
+            println!("D:  {:?}", compare_sentences(s1.split(" ").collect(), s2.split(" ").collect()));
         }
     }
 }
